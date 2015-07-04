829c4a5536698462e68f68c9752d98fb37d00fc1
raulrhonon <raulrhonon@gmail.com>
2015-07-04 14:43:03 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/author', function(req, res) {
-	res.render('/author', {title: 'Quiz'});
+	res.render('author', {title: 'Quiz'});
 });
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
